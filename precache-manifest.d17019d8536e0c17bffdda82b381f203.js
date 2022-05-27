4bc0084d2df22ac253a947a510c505e902a9a239
Nomah <3rdtimecoder@gmail.com>
2022-05-27 02:26:41 +0200

    Updates

---
+++ precache-manifest.d17019d8536e0c17bffdda82b381f203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14b96c84c9e8723e3e99e4633f8ef39",
+    "url": "/TravelDbn/index.html"
+  },
+  {
+    "revision": "6a1cb71a4090f35a3545",
+    "url": "/TravelDbn/static/css/main.9b211d67.chunk.css"
+  },
+  {
+    "revision": "8020aa597310408084e6",
+    "url": "/TravelDbn/static/js/2.68540829.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TravelDbn/static/js/2.68540829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1cb71a4090f35a3545",
+    "url": "/TravelDbn/static/js/main.16e5144f.chunk.js"
+  },
+  {
+    "revision": "57cb66b85c03849159b2",
+    "url": "/TravelDbn/static/js/runtime-main.54f6e9f2.js"
+  }
+]);
