42f46bcaced13f90fc1c6f715e658e7f368aa033
Nomah <3rdtimecoder@gmail.com>
2022-06-03 23:36:27 +0200

    changes made

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuqJTPB3kBft6ajIeAIXFXO2W9Hz_TVtU",
+  authDomain: "traveldbn.firebaseapp.com",
+  projectId: "traveldbn",
+  storageBucket: "traveldbn.appspot.com",
+  messagingSenderId: "906449088701",
+  appId: "1:906449088701:web:cd8ff1cb153573f12787d4",
+  measurementId: "G-QFSEVYM2SN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
